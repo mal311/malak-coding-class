9ae475bc5bbe1aa8796553e010c9df837b772fb4
mal311 <imaddahenmalak@gmail.com>
2025-02-14 06:33:12 +0100

    homework

---
+++ lesson 32/homework.py
@@ -0,0 +1,15 @@
+x = 5
+y = 6
+z = 9
+print("original x = ",x)
+print("original y = ",y)
+print("original z = ",z)
+print()
+
+m = x
+x = y
+y = z
+z = m
+print("x after swapping = ",x)
+print("y after swapping = ",y)
+print("z after swapping = ",z)
