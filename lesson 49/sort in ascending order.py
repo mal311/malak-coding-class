74f8b34b460b745fb93750cd9fdfbe8d50c326cb
mal311 <imaddahenmalak@gmail.com>
2025-04-16 17:48:16 +0100

    lesson 49

---
+++ lesson 49/sort in ascending order.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+data_type = [('name', '515'), ('class', int), ('height', float)]
+
+student_details = [("James", 5, 48.5), ("Nail", 6, 52.5), ("Paul", 5, 42.10), ("Pit", 5, 40.11)]
